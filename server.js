b4d2e180dec44c70eadd113320046efb1ef17436
Alex <ama1111@users.noreply.github.com>
2014-11-01 15:59:29 -0700

    Server parsing /links request body, making crossref request with it, and sending json results to client.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
-
+var needle = require('needle');
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
     res.send('Hello Word!')
@@ -10,6 +12,27 @@ app.get('/', function (req, res) {
 
 app.get('/links', function (req, res) {
     res.json({ message: 'message from links!' });
+})
+
+app.post('/links', function (req, res) {
+
+    console.log('got a post request');
+
+    console.log(req.body);
+    console.log('stringify:');
+    console.log(JSON.stringify(req.body));
+
+    needle.post(
+	'http://search.crossref.org/links',
+	JSON.stringify(req.body),
+	{},
+	function(err, resp) {
+	    console.log('returned from links');
+	    console.log(resp.body);
+	    res.json(resp.body);
+	}
+    );
+
 })
 
 var router = express.Router();
