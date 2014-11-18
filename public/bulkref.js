c77e775ca0648280fb1dfd4fdfa7b2e01f95b65b
Alex <ama1111@users.noreply.github.com>
2014-11-17 21:50:25 -0800

    Prototyping list group with hardcoded values.

---
+++ public/bulkref.js
@@ -0,0 +1,95 @@
+var i = -1;
+var titles;
+
+$(function() {
+
+    var template1 = '<div class="list-group-item"><h4 class="list-group-item-heading">';
+    var template2 = '</h4><img src="spinny.gif"></img>Loading...</div>';
+    //$('#result-list').append('<div class="list-group-item"><h4 class="list-group-item-heading">new insight into the strategy for nitrogen metabolism in plant cells</h4><img src="spinny.gif"></img>Loading...</div>');
+    var listItem = template1 + "beta vulgaris" + template2;
+    $('#result-list').append(listItem);
+  
+    $('#btn').click(function(e) {
+	console.log('clicked');
+
+	$("#citation").text("");
+	$("#doi").text("");
+
+	
+	var titleData;
+	if (i < 0) {
+	    var multiline = $('#title-area').val();
+	    titles = multiline.split('\n');
+
+	    //var title = $('#title-text').val();
+	    //var titleData = JSON.stringify([title]);
+	    i = 0;
+	}
+	else if (i >= titles.length-1) {
+	    alert('done');
+	    return;
+	}
+	var title = titles[i];
+	i++;
+
+	if (!title) {
+	    alert('empty title');
+	    return;
+	}
+	
+	titleData = JSON.stringify([title]);	      
+
+	$('#btn').text('Thinking...');
+	$('#btn').prop('disabled', true);
+	
+	$.ajax({
+	    url:'/links',
+	    type:"POST",
+	    data:titleData,
+	    contentType:"application/json; charset=utf-8",
+	    dataType:"json",
+	    success: function(data) {
+		console.log("data from ajax:");
+		console.log(data);
+
+		if (data.results[0].match) {
+
+		    var citation = data.results[0].citation;
+		    console.log(data.results[0].citation);
+		    $("#citation").text(citation);
+
+		    var doi = data.results[0].doi;
+		    console.log(doi);
+		    $("#doi").text(doi);
+
+		    var current = $("#doi-area").val();
+		    if (current.length > 0)
+		    {
+			current += "\n";
+		    }
+		    $("#doi-area").val(current + doi);
+
+		    $('#btn').text('Click me!');
+		    $('#btn').prop('disabled', false);
+
+		}
+		else {
+		    alert("Didn't find match for '" + title + "'");
+		    $('#btn').text('Click me!');
+		    $('#btn').prop('disabled', false);
+		}
+		
+	    },
+	    error: function(jqxhr, textStatus, errorThrown) {
+		console.log("textStatus:");
+		console.log(textStatus);
+		console.log("errorThrown:");
+		console.log(errorThrown);
+		alert("Unable to find DOI for '" + title + "'");
+		$('#btn').text('Click me!');
+		$('#btn').prop('disabled', false);
+	    }
+	});
+	
+    });
+});
