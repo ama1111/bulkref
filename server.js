35524c3c796e22ac8207fdeb249e423c8181447c
Alex <ama1111@users.noreply.github.com>
2014-12-15 17:44:32 -0800

    Fixing indents and semi-colons.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 // Using needle because 'request' doesn't allow raw form data.
 var needle = require('needle');
 var bodyParser = require('body-parser');
@@ -9,77 +9,77 @@ var request = require('request');
 app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static('public'));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get('/links', function (req, res) {
-    res.json({ message: 'message from links!' });
-})
+  res.json({ message: 'message from links!' });
+});
 
 app.post('/links', function (req, res) {
 
-    console.log('got a post request');
+  console.log('---------\ngot a post request');
 
-    console.log(req.body);
-    console.log('stringify:');
-    console.log(JSON.stringify(req.body));
+  console.log(req.body);
+  console.log('stringify:');
+  console.log(JSON.stringify(req.body));
 
-    needle.post(
-	'http://search.crossref.org/links',
-	JSON.stringify(req.body),
-	{},
-	function(err, resp) {
-	    console.log('returned from links');
-	    console.log(resp.body);
-	    console.log('match: ' + resp.body.results[0].match);
+  needle.post(
+    'http://search.crossref.org/links',
+    JSON.stringify(req.body),
+    {},
+    function(err, resp) {
+      console.log('returned from links');
+      console.log(resp.body);
+      console.log('match: ' + resp.body.results[0].match);
 
-	    if (resp.body.results[0].match) {
-	    var doiUrl = resp.body.results[0].doi;
-	    console.log('doiUrl:');
-	    console.log(doiUrl);
+      if (resp.body.results[0].match) {
+        var doiUrl = resp.body.results[0].doi;
+        console.log('doiUrl:');
+        console.log(doiUrl);
 
-	    var options = {
-		url: doiUrl,
-		headers: {
-		    'Accept': 'text/x-bibliography; style=apa'
-		    //'Accept': 'application/citeproc+json'
-		}
-	    };
-	    request(options, function(cError, cResponse, cBody) {
-		console.log('returned from doi using request');
-		console.log(cBody);
+        var options = {
+          url: doiUrl,
+          headers: {
+            'Accept': 'text/x-bibliography; style=apa'
+            //'Accept': 'application/citeproc+json'
+          }
+        };
+        request(options, function(cError, cResponse, cBody) {
+          console.log('returned from doi using request');
+          console.log(cBody);
 
-		//var citation = JSON.parse(dBody);
-		resp.body.results[0].citation = cBody;
+          //var citation = JSON.parse(dBody);
+          resp.body.results[0].citation = cBody;
 
-		options.headers = {
-		    'Accept': 'application/citeproc+json'
-		};
-		request(options, function(dError, dResponse, dBody) {
-		    console.log('returned from doi for json');
-		    console.log(dBody);
+          options.headers = {
+            'Accept': 'application/citeproc+json'
+          };
+          request(options, function(dError, dResponse, dBody) {
+            console.log('returned from doi for json');
+            console.log(dBody);
 
-		    var d = JSON.parse(dBody);
-		    console.log(d);
+            var d = JSON.parse(dBody);
+            console.log(d);
 
-		    resp.body.results[0].doi = d.DOI;
-		    res.json(resp.body);
-		});
-	    });
-	    }
-	    else {
-		console.log('did not match');
-		res.json(resp.body);
-	    }
+            resp.body.results[0].doi = d.DOI;
+            res.json(resp.body);
+          });
+        });
+      }
+      else {
+        console.log('did not match');
+        res.json(resp.body);
+      }
 
-	}
-    );
-})
+    }
+  );
+});
 
 var router = express.Router();
 
 var server = app.listen(app.get('port'), function () {
-    var host = server.address().address
-    var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port)
-})
+  console.log('Example app listening at http://%s:%s', host, port);
+});
